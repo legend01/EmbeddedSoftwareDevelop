cc2e58e508270b3734130a0a9b597291a81a8a73
happyCoding <lg95312lg@outlook.com>
2021-03-01 21:14:35 +0800

    [新增代码]1.增加BMS部分外部调用接口

---
+++ STM32F407STOREROOM/STM32F407_FreeRTOS/App/BMSBMS/BMS_BMS.c
@@ -0,0 +1,61 @@
+/*
+ * @Description: 
+ * @Author: HLLI8
+ * @Date: 2021-03-01 11:10:29
+ * @LastEditTime: 2021-03-01 21:13:04
+ * @LastEditors: HLLI8
+ */
+#include "BMS_BMS.h"
+
+
+
+void BMSmanager_Init(void){
+    Ringbuff_setEmpty();
+    J1939_connect_send_clear();
+    J1939_connect_clear();
+    PGN_MessageRcv_clear();
+}
+
+void BMS_Send_message(PGNTypeSend ePGNTypeSend, char *data){
+    if(data != NULL)
+    {
+        for (int i = 0; i < PGNInfoSend[ePGNTypeSend].dataLen; i++)
+        {
+            BMSmanager.msgSendData[i] = data[i] & 0xff;
+        }
+        PushMsgToRBUFF(ePGNTypeSend, BMSmanager.msgSendData);
+    }
+    
+}
+/***************************************************************************
+功能: 通过PGN的索引号读取得到的实际值PGN的数据部分
+参数: 1 PGN 索引    2 读取到的数据指针
+返回: -1为错误 1 为数据无效 messageData_len 为数据正常更新并且被读取
+*****************************************************************************/
+int BMS_Get_message(PGNTypeRcv ePGNTypeRcv, char** messageData)
+{
+    uint8_t messageData_len = 0;
+
+    if(ePGNTypeRcv >= TPCM_rcv)//判断PGN范围
+    {
+        return -1;
+    }
+
+    messageData_len = PGNInfoRcv[ePGNTypeRcv].dataLen;
+    
+    if(messageData_len > messageData_MAXnum)        //判断PGN的最大长度是否大于最大数据
+    {
+        return -1;
+    }
+    
+    if(PGN_MessageRcv[ePGNTypeRcv].valid == 0) //所查看的PGN的数据的有效性,如果无效则直接返回0
+    {
+        return 0;
+    }
+    
+    *messageData = PGN_MessageRcv[ePGNTypeRcv].data;  //直接赋值指针
+    
+    PGN_MessageRcv[ePGNTypeRcv].valid = 0; //数据读取成功后无效
+    
+    return PGNInfoRcv[ePGNTypeRcv].dataLen;
+}
---
+++ STM32F407STOREROOM/STM32F407_FreeRTOS/App/BMSBMS/BMS_BMS.h
@@ -0,0 +1,97 @@
+/*
+ * @Description: 
+ * @Author: HLLI8
+ * @Date: 2021-03-01 11:10:35
+ * @LastEditTime: 2021-03-01 15:56:32
+ * @LastEditors: HLLI8
+ */
+#ifndef __BMS_BMS_H__
+#define __BMS_BMS_H__
+
+#include "BMS_J1939x.h"
+#include "pub_data.h"
+
+#define messageData_MAXnum 60
+
+
+typedef enum BMS_stage           //发
+{
+	Handshake_rcvCRM_norec = 0, //未识别                 
+	Handshake_sendBRM,                 //               
+	Handshake_rcvCRM_rec, //识别                         
+    Handshake_rcvCHM,                                     
+	Handshake_sendBHM,
+    
+	ParConfig_sendBCP,
+	ParConfig_rcvCTS_CML,
+	ParConfig_sendBRO,
+	ParConfig_rcvCRO,
+
+	charging_sendBCS,
+	charging_sendBCL,
+	charging_rcvCCS,
+	charging_sendBSM,
+	charging_sendBMV,
+	charging_sendBMT,
+	charging_sendBSP,
+	
+	chargend_rcvCST,
+	chargend_sendBST,
+	chargend_sendBSD,
+	chargend_rcvCSD,
+}BMS_stage;
+
+typedef enum BMS_flag         //充电阶段标志
+{
+	BMS_ready 	= 0, 	//充电前的准备	
+	BMS_Handshake,		//充电握手
+	BMS_Config,			//配置
+	BMS_Charging,		//充电
+	BMS_END,			//结束
+	BMS_Finish,			//关闭
+	BMS_ErrorTimeout,	//错误或者超时状态
+	
+	BMS_StateMax,       
+}BMS_flag;
+
+typedef enum
+{
+    BMS_NOREC = 0,
+    BMS_2011,
+    BMS_2015,
+}PROTOL;
+
+#define SendDataLen_Max 50
+typedef struct BMS_Manage
+{
+	u16 	BMS_needV;        //BMS实时要求充电机的电压
+	u16 	BMS_needI;        //BMS实时要求充电机的电流 
+	u8      BMS_needM;        //BMS要求充电机的充电模式
+	u8      Stop_Mod:2;       //充电连接停止类型 0正常通信  1被动停止  2为主动停止  
+    u8      BMS_relink:3;     // BMS超时重新连接次数
+	u16 	BMSTime_rcv;      //定时器计时ms
+	u16     MsgSend_cycle;    //一个信息每隔多少MS发送一次
+	u16     MsgSend_waitTime; //定时器计数等待时间MS
+	u16     charg_Curmeasure; //BMS测量电流值
+	u16	charg_Volmeasure; //BMS测量电压值
+        u8      charg_NowSOC;     //当前SOC实际值
+        u16     BAT_TMleft;       //估算剩余充电时间
+        u8      BMS_readyStat;    //准备是否就绪
+        u8      ChargReady_YESNO; //充电桩准备是否就绪
+        u8      BMS_Stop;     //  0:正常运行 1:BMS主动停止
+	u8      MsgSend_Count;    
+	char* 	messageData;      //接收的PGN数据缓存
+    char    msgSendData[SendDataLen_Max];  //发送的数据缓存  
+    bool        BMS_STOP;
+    
+	PROTOL	BMS_V;		// 0为2015年薪国标 1 为2011年老国标
+    u16     BMS_PREV;
+	BMS_flag  Chargeworkflg;
+	BMS_stage Charge_sata ;    //充电过程中 充电状态
+}sBMS_Manage,*psBMS_Manage;
+extern sBMS_Manage BMSmanager;
+
+int BMS_Get_message(PGNTypeRcv ePGNTypeRcv, char** messageData);
+void BMS_Send_message(PGNTypeSend ePGNTypeSend, char *data);
+void BMSmanager_Init(void);
+#endif // !__BMS_BMS_H__
